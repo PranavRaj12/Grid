3c19cb1596302b37b45651030afc00904c3b0d10
PranavRaj12 <rajpranav123@gmail.com>
2021-05-08 00:26:39 +0530

    updated

--- app/src/main/java/com/pranav/raj/grid/MainActivity.kt
+++ app/src/main/java/com/pranav/raj/grid/MainActivity.kt
@@ -4,21 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.google.android.material.textview.MaterialTextView
 import com.pranav.raj.grid_module.ConnectionStatus
 
-private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val connectionStatus = ConnectionStatus()
+            val status = findViewById<MaterialTextView>(R.id.statusText) as MaterialTextView
 
-        if (!connectionStatus.isConnected(this)){
-            Toast.makeText(this,"Network error",Toast.LENGTH_LONG).show()
-        }
-        else{
-            Log.i(TAG, "onCreate: "+ "Connected")
-        }
+            val connectionStatus = ConnectionStatus()
+            if (!connectionStatus.isConnected(this)){
+                status.setText("Network Error!")
+            }
+            else{
+                status.setText("Connected.")
+            }
     }
 }
